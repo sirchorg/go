de91f40b5d411cce6a4a966546afe3f112840a4f
alex breadman <alex@cpu.host>
2023-03-24 16:41:36 +0000

    add model

--- common/http.go
+++ common/http.go
@@ -3,8 +3,27 @@ package common
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io"
+	"net/http"
 )
+
+func (app *App) ParseJSON(r *http.Request, dst interface{}) error {
+	b, err := io.ReadAll(r.Body)
+	if r.Body != nil {
+		defer r.Body.Close()
+	}
+	if err != nil {
+		return err
+	}
+	if dst != nil {
+		if err := json.Unmarshal(b, dst); err != nil {
+			return err
+		}
+		return nil
+	}
+	return errors.New("no destination for ParseJSON")
+}
 
 func (app *App) GetJSON(url string, dst interface{}) error {
 	resp, err := app.httpClient.Get(url)
