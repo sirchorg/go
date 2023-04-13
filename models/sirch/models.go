c4a5f50b164e1dfc8307e934814e032d92dbefe6
alex breadman <alex@cpu.host>
2023-04-14 00:58:49 +0100

    mode;s

---
+++ models/sirch/models.go
@@ -0,0 +1,18 @@
+package models
+
+type Organization struct {
+	Name string
+}
+
+type Feed struct {
+	Type      string
+	URL       string
+	Frequency int
+	Checked   int64
+}
+
+type FeedItem struct {
+	Title string
+	Link  string
+	Time  int64
+}
