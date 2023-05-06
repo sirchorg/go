cf8b9efd22636e917c60767f4b8bda30ca5fab24
alex breadman <3dukltd@gmail.com>
2023-05-06 22:08:27 +0100

    fix ai

--- common/ai.go
+++ common/ai.go
@@ -1,53 +1,52 @@
 package common
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"strings"
 )
 
-func (app *App) ParseContentForObjectOrArrayJSON(content string, dst interface{}) error {
-
-	// trim the content to only raw json
-	inO := strings.Index(content, "{")
-	inA := strings.Index(content, "[")
-	outO := strings.LastIndex(content, "}")
-	outA := strings.LastIndex(content, "]")
-
-	if app.debugMode {
-		println(">>", inO, string(content[inO]), inA, string(content[inA]))
-		println(">>", outO, string(content[outO]), outA, string(content[outA]))
+func checkContentObject(content string) (string, error) {
+	in := strings.Index(content, "{")
+	out := strings.LastIndex(content, "}")
+	if in < 0 || out < 1 {
+		return "", fmt.Errorf("cant find object %d %d", in, out)
 	}
+	return content[in : out+1], nil
+}
 
-	if (inO < 0 || outO < 0) && (inA < 0 || outA < 0) {
-		return fmt.Errorf("out of bounds error, no array or object? %d, %d, %d, %d", inO, inA, outO, outA)
+func checkContentArray(content string) (string, error) {
+	in := strings.Index(content, "[")
+	out := strings.LastIndex(content, "]")
+	if in < 0 || out < 1 {
+		return "", fmt.Errorf("cant find array %d %d", in, out)
 	}
+	return content[in : out+1], nil
+}
 
-	var in, out int
-	if inO > 0 && inO > inA {
-		in = inO
-		out = outO
-		if app.debugMode {
-			println("receiving object")
-		}
-	} else {
-		in = inA
-		out = outA
-		if app.debugMode {
-			println("receiving array")
-		}
-	}
+func (app *App) ParseContentForObject(content string, dst interface{}) error {
 
-	// make sure we ignore subordinate drivel (safely)
-	if out >= len(content) {
-		return errors.New("preventing panid when trimming content")
+	j, err := checkContentObject(content)
+	if err != nil {
+		return err
 	}
-	j := content[in : out+1]
 
 	if app.debugMode {
 		println("DEBUG", j)
 	}
 
-	return json.Unmarshal([]byte(j), dst)
+	return app.UnmarshalJSON([]byte(j), dst)
+}
+
+func (app *App) ParseContentForArray(content string, dst interface{}) error {
+
+	j, err := checkContentArray(content)
+	if err != nil {
+		return err
+	}
+
+	if app.debugMode {
+		println("DEBUG", j)
+	}
+
+	return app.UnmarshalJSON([]byte(j), dst)
 }
