245c4dbe3fa983820f1cf1e6edb04bb88a6d3c70
alex breadman <3dukltd@gmail.com>
2023-06-15 22:29:00 +0100

    GCS methods

--- common/clients_gcp_gcs.go
+++ common/clients_gcp_gcs.go
@@ -7,17 +7,18 @@ import (
 	"io"
 
 	"cloud.google.com/go/storage"
+	"google.golang.org/api/iterator"
 )
 
-func (self *GCPClients) GetObjectAndUnmarshal(bucket *storage.BucketHandle, objectName string, dst interface{}) error {
-	b, err := self.GetObjectGCS(bucket, objectName)
+func (self *GCPClients) GetObjectAndUnmarshal(ctx context.Context, bucket *storage.BucketHandle, objectName string, dst interface{}) error {
+	b, err := self.GetObjectGCS(ctx, bucket, objectName)
 	if err != nil {
 		return err
 	}
 	return json.Unmarshal(b, dst)
 }
 
-func (self *GCPClients) GetObjectGCS(bucket *storage.BucketHandle, objectName string) ([]byte, error) {
+func (self *GCPClients) GetObjectGCS(ctx context.Context, bucket *storage.BucketHandle, objectName string) ([]byte, error) {
 	r, err := bucket.Object(objectName).NewReader(context.Background())
 	if err != nil {
 		return nil, fmt.Errorf("storage.GetObject: %w", err)
@@ -30,4 +31,22 @@ func (self *GCPClients) GetObjectGCS(bucket *storage.BucketHandle, objectName st
 		return nil, fmt.Errorf("storage.GetObject: %w", err)
 	}
 	return b, nil
+}
+
+// ListObjectGCS returns the contents of the directory with the given prefix
+func (self *GCPClients) ListObjectGCS(ctx context.Context, bucketName, prefix string) (names []string, err error) {
+
+	it := self.GCS().Bucket(bucketName).Objects(ctx, &storage.Query{Prefix: prefix})
+	for {
+		var attrs *storage.ObjectAttrs
+		attrs, err = it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return
+		}
+		names = append(names, attrs.Name)
+	}
+	return
 }
